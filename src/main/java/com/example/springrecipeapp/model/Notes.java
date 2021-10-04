0a401d3d588817a638e9447e41a98adb24e4a2c9
patricia2193 <patricia21.1993@gmail.com>
2021-10-04 19:07:40 +0300

    excluding hashcode

--- src/main/java/com/example/springrecipeapp/model/Notes.java
+++ src/main/java/com/example/springrecipeapp/model/Notes.java
@@ -1,10 +1,12 @@
 package com.example.springrecipeapp.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 

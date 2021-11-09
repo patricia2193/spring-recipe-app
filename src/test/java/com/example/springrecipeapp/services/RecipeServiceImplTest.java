8f11ed85d60906e9e5fd48ab473efa19a771e6bf
Patricia.Petruta <patricia.petruta@orange.com>
2021-11-09 11:46:07 +0200

    display recipe

--- src/test/java/com/example/springrecipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/example/springrecipeapp/services/RecipeServiceImplTest.java
@@ -2,13 +2,13 @@ package com.example.springrecipeapp.services;
 
 import com.example.springrecipeapp.model.Recipe;
 import com.example.springrecipeapp.repositories.RecipeRepository;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,5 +40,20 @@ class RecipeServiceImplTest {
 
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void getRecipeByIdTest() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull(recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
     }
 }

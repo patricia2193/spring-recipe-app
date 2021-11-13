fd30e878424fcd9378316bc409b1c2878118ea9b
Patricia.Petruta <patricia.petruta@orange.com>
2021-11-13 17:24:46 +0200

    update recipe

--- src/main/java/com/example/springrecipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/example/springrecipeapp/services/RecipeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.springrecipeapp.services;
 
+import com.example.springrecipeapp.commands.RecipeCommand;
+import com.example.springrecipeapp.converters.RecipeCommandToRecipe;
+import com.example.springrecipeapp.converters.RecipeToRecipeCommand;
 import com.example.springrecipeapp.model.Recipe;
 import com.example.springrecipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -14,9 +18,13 @@ import java.util.Set;
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     @Override
@@ -40,5 +48,21 @@ public class RecipeServiceImpl implements RecipeService {
         }
 
         return recipeOptional.get();
+    }
+
+    @Override
+    @Transactional
+    public RecipeCommand findCommandById(Long l) {
+        return recipeToRecipeCommand.convert(findById(l));
+    }
+
+    @Override
+    @Transactional
+    public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(command);
+
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        log.debug("Saved RecipeId:" + savedRecipe.getId());
+        return recipeToRecipeCommand.convert(savedRecipe);
     }
 }
--- src/test/java/com/example/springrecipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/example/springrecipeapp/services/RecipeServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.example.springrecipeapp.services;
 
+import com.example.springrecipeapp.converters.RecipeCommandToRecipe;
+import com.example.springrecipeapp.converters.RecipeToRecipeCommand;
 import com.example.springrecipeapp.model.Recipe;
 import com.example.springrecipeapp.repositories.RecipeRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,11 +23,17 @@ class RecipeServiceImplTest {
     @Mock
     RecipeRepository recipeRepository;
 
+    @Mock
+    RecipeToRecipeCommand recipeToRecipeCommand;
+
+    @Mock
+    RecipeCommandToRecipe recipeCommandToRecipe;
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        recipeService = new RecipeServiceImpl(recipeRepository);
+        recipeService = new RecipeServiceImpl(recipeRepository, recipeCommandToRecipe, recipeToRecipeCommand);
     }
 
     @Test

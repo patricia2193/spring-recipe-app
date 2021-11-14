edcdc890ef6f48319ddf6018f66e24f11b6698ac
Patricia.Petruta <patricia.petruta@orange.com>
2021-11-14 11:54:41 +0200

    update ingredient

--- src/main/java/com/example/springrecipeapp/converters/IngredientCommandToIngredient.java
+++ src/main/java/com/example/springrecipeapp/converters/IngredientCommandToIngredient.java
@@ -2,6 +2,7 @@ package com.example.springrecipeapp.converters;
 
 import com.example.springrecipeapp.commands.IngredientCommand;
 import com.example.springrecipeapp.model.Ingredient;
+import com.example.springrecipeapp.model.Recipe;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
@@ -24,6 +25,14 @@ public class IngredientCommandToIngredient implements Converter<IngredientComman
 
         final Ingredient ingredient = new Ingredient();
         ingredient.setId(source.getId());
+
+        if(source.getRecipeId() != null){
+            Recipe recipe = new Recipe();
+            recipe.setId(source.getRecipeId());
+            ingredient.setRecipe(recipe);
+            recipe.addIngredient(ingredient);
+        }
+
         ingredient.setAmount(source.getAmount());
         ingredient.setDescription(source.getDescription());
         ingredient.setUnitOfMeasure(uomConverter.convert(source.getUnitOfMeasure()));
---
+++ src/main/java/com/example/springrecipeapp/services/UnitOfMeasureService.java
@@ -0,0 +1,10 @@
+package com.example.springrecipeapp.services;
+
+import com.example.springrecipeapp.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}

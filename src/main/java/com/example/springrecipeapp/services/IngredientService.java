1ec8febbf8c7983b5ca3973211feed2859acb93b
Patricia.Petruta <patricia.petruta@orange.com>
2021-11-14 11:17:58 +0200

    show ingredients

---
+++ src/main/java/com/example/springrecipeapp/services/IngredientService.java
@@ -0,0 +1,8 @@
+package com.example.springrecipeapp.services;
+
+import com.example.springrecipeapp.commands.IngredientCommand;
+
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}

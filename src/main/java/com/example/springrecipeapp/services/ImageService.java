ea2cdd37a0e80b1793ef8b08a1c021b79505ca8d
Petruta <patricia21.1993@gmail.com>
2021-12-28 16:20:16 +0200

    adding image controller and service

---
+++ src/main/java/com/example/springrecipeapp/services/ImageService.java
@@ -0,0 +1,8 @@
+package com.example.springrecipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
